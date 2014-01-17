2931eb26d0019bb34bae6a7fc848b745d80538f7
Elemental-Code <elemcreations@gmail.com>
2014-01-17 23:20:39 +0200

    Folder restructure, basics for frmMain.

--- src/Program.cs
+++ src/Program.cs
@@ -16,7 +16,7 @@ namespace UniLauncher
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
